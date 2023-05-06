aa4c887889bb3405570096b833d3892dc82ef2e6
Ebla Ekerdiker <eblaekerdiker@gmail.com>
2023-05-07 01:06:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(listOfDrivers ,name){
+    return listOfDrivers.filter(function (driverName){
+        return driverName.toLowerCase() === name.toLowerCase();
+    })
+}
+
+function fuzzyMatch(listOfDrivers,letters){
+    return listOfDrivers.filter(function(driver){
+        return driver.toLowerCase().substring(0,letters.length) === letters.toLowerCase()
+    })
+}
+
+function matchName(listOfDrivers, check){
+    return listOfDrivers.filter(function(driver){
+        return driver.name.toLowerCase() === check.toLowerCase()
+    })
+}
